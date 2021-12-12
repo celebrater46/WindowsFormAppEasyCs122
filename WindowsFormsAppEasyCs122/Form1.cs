8b5d9db1411779fdc1429dafb9205d05ee791ec5
hideru <jota.16.stigma.world@gmail.com>
2021-12-13 00:40:41 +0900

    all tests succeeded

--- WindowsFormsAppEasyCs122/Form1.cs
+++ WindowsFormsAppEasyCs122/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace WindowsFormsAppEasyCs122
 {
     public partial class Form1 : Form
     {
+        private ListBox lbx;
         public Form1()
         {
             InitializeComponent();
+            this.Text = "Get All Data";
+            this.Width = 300;
+            this.Height = 200;
+
+            lbx = new ListBox();
+            lbx.Dock = DockStyle.Fill;
+
+            var cars = new[]
+            {
+                new { num = 2, name = "Benz" },
+                new { num = 3, name = "Ferrari" },
+                new { num = 4, name = "Fuso" },
+            };
+            
+            XDocument doc = XDocument.Load("C:\\Users\\Enin\\RiderProjects\\WindowsFormsAppEasyCs122\\WindowsFormsAppEasyCs122\\Sample.xml");
+
+            // IEnumerable qry = from K in doc.Descendants("car") select K;
+            // IEnumerable qry = from K in doc.Descendants("car") where (string)K.Attribute("country") == "Japan" select K;
+            // IEnumerable qry = from K in doc.Descendants("car") where (int)K.Attribute("id") >= 1005 select K.Element("name").Value;
+            IEnumerable qry = from K in doc.Descendants("car") orderby(int)K.Attribute("id") select K.Element("name").Value;
+
+            foreach (var tmp in qry)
+            {
+                lbx.Items.Add(tmp);
+            }
+
+            lbx.Parent = this;
         }
     }
 }
